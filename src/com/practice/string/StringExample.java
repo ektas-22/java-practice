fa940c50ff19167bf62622e17a89a219930d1b83
Ekta Sollet <ektasollet12@gmail.com>
2026-04-30 13:02:59 +0530

    String Example class

--- src/com/practice/string/StringExample.java
+++ src/com/practice/string/StringExample.java
@@ -3,6 +3,9 @@ package com.practice.string;
 public class StringExample {
 	public static void main(StringExample[] args) {
 		
+		//Example 1 - creating java string using new keyword
+		String str1=new String("Hello");
+		System.out.println(str1);
 		
 	}
 
