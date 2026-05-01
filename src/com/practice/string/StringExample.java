a11faa2a434f1d7f92f6c3e697b4b22b73030289
Ekta Sollet <ektasollet12@gmail.com>
2026-05-01 12:46:54 +0530

    String example class update

--- src/com/practice/string/StringExample.java
+++ src/com/practice/string/StringExample.java
@@ -7,6 +7,13 @@ public class StringExample {
 		String str1 = new String("Hello");
 		System.out.println(str1);
 
+		// Ways of Creating a Java String
+		// 1. String literal (Static Memory)
+		String str = "Hello World";
+		System.out.println(str);
+		// 2. Using new keyword (Heap Memory)
+		String stra = new String("Hello");
+		System.out.println(stra);
+		
 	}
-
 }
