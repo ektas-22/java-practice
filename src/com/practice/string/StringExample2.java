f0fa3dc337a6e6b25c57b1c6196f79373e01f5f8
Ekta Sollet <ektasollet12@gmail.com>
2026-05-02 16:29:21 +0530

    String Example Update

--- src/com/practice/string/StringExample2.java
+++ src/com/practice/string/StringExample2.java
@@ -3,6 +3,19 @@ package com.practice.string;
 public class StringExample2 {
 	public static void main(String[] args) {
 
+		// Example Demonstrating Immutability
+		String str1 = "Hello";
+		String str2 = "Hello";
+		System.out.println("str1=str2 - " + (str1 == str2));
+		str1 = str1.concat(" World");
+		System.out.println("String 1 - " + str1);
+		System.out.println("String 2 - " + str2);
+		System.out.println("str1=str2 - " + (str1 == str2));
+
+		String str3 = new String("Hello");
+		System.out.println("str2==str3 - " + (str2 == str3));
+		System.out.println("str2.equals(s3) - " + str2.equals(str3));
+
 	}
 
 }
