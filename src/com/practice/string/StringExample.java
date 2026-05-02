177f37a166a153f9a1a957e120909881364e6f7d
Ekta Sollet <ektasollet12@gmail.com>
2026-05-02 12:45:23 +0530

    String Example Update

--- src/com/practice/string/StringExample.java
+++ src/com/practice/string/StringExample.java
@@ -7,31 +7,32 @@ public class StringExample {
 
 		// Example 1 - creating java string using new keyword
 		String str1 = new String("Hello");
-		System.out.println(str1);
+		System.out.println("Using new operator - " + str1);
 
 		// Ways of Creating a Java String
 		// 1. String literal (Static Memory)
 		String str = "Hello World";
-		System.out.println(str);
+		System.out.println("Using string literal - " + str);
 		// 2. Using new keyword (Heap Memory)
 		String stra = new String("Hello");
-		System.out.println(stra);
+		System.out.println("String using new keyword - " + stra);
 
 		// CharSequence Interface
 		// Classes that implement charSequence include
 		// 1. String
 		String str2 = "Hello World!";
 		String str3 = new String("John");
-		System.out.println(str2 + " " + str3);
+		System.out.println(str2);
+		System.out.println(str3);
 
 		// 2. StringBuffer
 		StringBuffer stb = new StringBuffer("Hello String Buffer");
-		System.out.println(stb);
+		System.out.println("String buffer - " + stb);
 
 		// 3. StringBuilder
 		StringBuilder stb2 = new StringBuilder();
 		stb2.append("Hello");
-		System.out.println(stb2);
+		System.out.println("String builder - " + stb2);
 
 		// 4. StringTokenizer
 		StringTokenizer strz = new StringTokenizer("Hello World Example");
@@ -41,6 +42,18 @@ public class StringExample {
 		String str4 = "Sachin";
 		str4.concat("Tendulkar");
 		System.out.println(str4);
+
+		// Reference explicitly
+		String str5 = "Sachin";
+		str5 = str5.concat(" Tendulkar");
+		System.out.println(str5);
+
+		// String using others
+		char character[] = { 'H', 'i', 'i' };
+		String str6 = new String(character);
+		System.out.println("String using character - " + str6);
+		String str7 = new String(str6);
+		System.out.println("String using another string - " + str7);
 
 	}
 }
---
+++ src/com/practice/string/StringExample2.java
@@ -0,0 +1,8 @@
+package com.practice.string;
+
+public class StringExample2 {
+	public static void main(String[] args) {
+
+	}
+
+}
