8399da3d3f632cbd87a8849bdc034d63ce623e7a
Ekta Sollet <ektasollet12@gmail.com>
2026-05-02 11:06:19 +0530

    String Example Update

--- src/com/practice/string/StringExample.java
+++ src/com/practice/string/StringExample.java
@@ -1,5 +1,7 @@
 package com.practice.string;
 
+import java.util.StringTokenizer;
+
 public class StringExample {
 	public static void main(String[] args) {
 
@@ -14,6 +16,31 @@ public class StringExample {
 		// 2. Using new keyword (Heap Memory)
 		String stra = new String("Hello");
 		System.out.println(stra);
-		
+
+		// CharSequence Interface
+		// Classes that implement charSequence include
+		// 1. String
+		String str2 = "Hello World!";
+		String str3 = new String("John");
+		System.out.println(str2 + " " + str3);
+
+		// 2. StringBuffer
+		StringBuffer stb = new StringBuffer("Hello String Buffer");
+		System.out.println(stb);
+
+		// 3. StringBuilder
+		StringBuilder stb2 = new StringBuilder();
+		stb2.append("Hello");
+		System.out.println(stb2);
+
+		// 4. StringTokenizer
+		StringTokenizer strz = new StringTokenizer("Hello World Example");
+		System.out.println(strz);
+
+		// Immutable String in Java
+		String str4 = "Sachin";
+		str4.concat("Tendulkar");
+		System.out.println(str4);
+
 	}
 }
