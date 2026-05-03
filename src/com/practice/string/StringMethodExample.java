198d136977f259d647de51112924cf9459c89420
Ekta Sollet <ektasollet12@gmail.com>
2026-05-03 18:21:00 +0530

    String method example update

--- src/com/practice/string/StringMethodExample.java
+++ src/com/practice/string/StringMethodExample.java
@@ -2,6 +2,33 @@ package com.practice.string;
 
 public class StringMethodExample {
 	public static void main(String[] args) {
-		
+
+		// Example
+		String str = "Geeks for Geeks";
+		System.out.println("Length - " + str.length());
+		System.out.println("Upper case - " + str.toUpperCase());
+		System.out.println("Sub string - " + str.substring(2, 7));
+
+		// 1.
+
+		// 2.
+
+		// 3.
+
+		// 4.
+
+		// 5.
+
+		// 6.
+
+		// 7.
+
+		// 8.
+
+		// 9.
+
+		// 10.
+
+		// 11.
 	}
 }
