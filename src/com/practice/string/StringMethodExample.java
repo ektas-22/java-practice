4788a50dbf77a66bb012275165c914980abbb8a5
Ekta Sollet <ektasollet12@gmail.com>
2026-05-04 11:36:25 +0530

    Java String Method Update

--- src/com/practice/string/StringMethodExample.java
+++ src/com/practice/string/StringMethodExample.java
@@ -9,26 +9,44 @@ public class StringMethodExample {
 		System.out.println("Upper case - " + str.toUpperCase());
 		System.out.println("Sub string - " + str.substring(2, 7));
 
-		// 1.
+		// 1. int length() Method
+		String str1 = "Hello World";
+		System.out.println("Length - " + str1.length());
 
-		// 2.
+		// 2. charAt(int i) Method
 
-		// 3.
+		// 3.String substring(int i) Method
 
-		// 4.
+		// 4. String substring(int i, int j) Method
 
-		// 5.
+		// 5. String concat( String str) Method
 
-		// 6.
+		// 6. int indexOf(String s) Method
 
-		// 7.
+		// 7. int indexOf(String s, int i) Method
 
-		// 8.
+		// 8. int lastIndexOf(String s) Method
 
-		// 9.
+		// 9. boolean equals(Object otherObj) Method
 
-		// 10.
+		// 10. boolean equalsIgnoreCase(String anotherString) Method
 
-		// 11.
+		// 11. int compareTo(String anotherString) Method
+
+		// 12. int compareToIgnoreCase(String anotherString) Method
+
+		// 13. String toLowerCase() Method
+
+		// 14. String toUpperCase() Method
+
+		// 15. String trim() Method
+
+		// 16. String replace(char oldChar, char newChar) Method
+
+		// 17. boolean contains(CharSequence sequence) Method
+
+		// 18. char[] toCharArray() Method
+
+		// 19. boolean startsWith(String prefix) Method
 	}
 }
