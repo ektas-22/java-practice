336a225415dd6fffd65189d85256644e831e2dd1
Ekta Sollet <ektasollet12@gmail.com>
2026-05-07 16:44:32 +0530

    StringBuffer example cocmmit

--- src/com/practice/string/StringBufferExample.java
+++ src/com/practice/string/StringBufferExample.java
@@ -3,6 +3,7 @@ package com.practice.string;
 public class StringBufferExample {
 	public static void main(String[] args) {
 
+		// StringBuffer to concatenate strings.
 		StringBuffer s = new StringBuffer();
 		s.append("Hello");
 		s.append(" ");
@@ -10,6 +11,32 @@ public class StringBufferExample {
 
 		String str = s.toString();
 		System.out.println(str);
+
+		// Constructors of StringBuffer Class
+		StringBuffer sb = new StringBuffer();
+		sb.append("Hello");
+		System.out.println("Default constructor: " + sb);
+
+		StringBuffer sb1 = new StringBuffer(50);
+		sb1.append("Java Programming");
+		System.out.println("int size consttructor: " + sb1);
+
+		StringBuffer sb2 = new StringBuffer("Welcome");
+		sb2.append(" to Java.");
+		System.out.println("String Constructor: " + sb2);
+		
+		//insert() method
+		
+		//replace() method
+		
+		//delete() Method
+		
+		//reverse() Method
+		
+		//capacity() Method
+		
+		
+
 	}
 
 }
