731fbce085f9476cf1d0caee657a386a571a07da
Ekta Sollet <ektasollet12@gmail.com>
2026-05-12 18:32:08 +0530

    StringBuilder class update.

--- src/com/practice/string/StringBuilderExample.java
+++ src/com/practice/string/StringBuilderExample.java
@@ -8,9 +8,14 @@ public class StringBuilderExample {
 		System.out.println("Initial StringBuilder: " + s);
 		s.append(" is a programming language");
 		System.out.println("After append: " + s);
-		
-		//StringBuilder Constructors
-		
+
+		// StringBuilder Constructors
+		StringBuilder sb1 = new StringBuilder();
+		sb1.append("Hello");
+		System.out.println("Empty Constructor: " + sb1);
+		StringBuilder sb2 = new StringBuilder(50);
+		sb2.append("This has initial capacity 50");
+		System.out.println("Capacity: " + sb2);
 	}
 
 }
