8b250fe8b0c131a97462c5a54248d857c3f46977
Ekta Sollet <ektasollet12@gmail.com>
2026-05-12 18:18:16 +0530

    OOPs example commit

--- src/com/practice/oops/OopsExample.java
+++ src/com/practice/oops/OopsExample.java
@@ -1,7 +1,12 @@
 package com.practice.oops;
 
+import java.util.Scanner;
+
 public class OopsExample {
 	public static void main(String[] args) {
 
+		Scanner src = new Scanner(System.in);
+		
+		src.close();
 	}
 }
