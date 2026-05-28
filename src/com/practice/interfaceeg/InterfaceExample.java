574686a10f770a68e5f417cb1f3416618f2acbcd
Ekta S <ektasollet12@gmail.com>
2026-05-28 13:02:27 +0530

    Interface example update

--- src/com/practice/interfaceeg/InterfaceExample.java
+++ src/com/practice/interfaceeg/InterfaceExample.java
@@ -1,18 +1,71 @@
 package com.practice.interfaceeg;
 
-interface TestInterface{
+interface TestInterface {
 	final int a = 10;
+
 	void display();
 }
-class TestClass implements TestInterface{
+
+class TestClass implements TestInterface {
 
 	@Override
 	public void display() {
 		System.out.println("Geeks");
-		
+
 	}
-	
+
 }
+
+interface Vehicle {
+	void gearUp();
+
+	void increaseSpeed();
+}
+
+class Bicycle implements Vehicle {
+
+	@Override
+	public void gearUp() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void increaseSpeed() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class Bike implements Vehicle {
+
+	@Override
+	public void gearUp() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void increaseSpeed() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+interface Add {
+
+}
+
+interface Subtract {
+
+}
+
+class Calc implements Add, Subtract {
+
+}
+
 public class InterfaceExample {
 	public static void main(String[] args) {
 		TestClass t = new TestClass();
